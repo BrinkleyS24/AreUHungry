351f1eb54371df6fd65fd9c6475d488d9fe0de80
Stacey Brinkley <brinkleystacey12@gmail.com>
2021-12-14 16:04:35 -0500

    fixed my list of ingredients

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const URL = 'https://www.themealdb.com/api/json/v1/1/random.php';
 const toDO = [];
 
+
 // element references
 const $getMeal = $('#getMeal');
 const $dislike = $('.dislike');
@@ -9,44 +10,62 @@ const $like = $('like');
 
 // // event listeners
 $getMeal.on('click', handleGetMeal);
-// $dislike.on('click', skipMeal)
+
 
 // functions
+
 
 function handleGetMeal(evt) {
 
     $.ajax(URL).then(function (data) {
         render(data);
-        console.log(data.meals[0])
     }, function (error) {
-        console.log('something went wrong')
+        console.log('something went wrong');
         console.log(error);
-    })
+    });
+    $(this).remove();
 };
 
 
+
 function render(meal) {
+
+    let ingredients = [];
+    
+    for (let i = 1; i <= 20; i++) {
+        if (meal.meals[0]["strIngredient" + i]) {
+            ingredients.push(
+                `${meal.meals[0][`strIngredient${i}`]} - ${meal.meals[0][`strMeasure${i}`]}`
+            );
+        } else {
+            break;
+        }
+    }
+
+
     $('main').html(`
-    <h3>Title: ${meal.meals[0].strMeal}</h3>
+    <h3>Meal: ${meal.meals[0].strMeal}</h3>
     <img src="${meal.meals[0].strMealThumb}" alt="${meal.meals[0].strMeal}">
+    <p>Ingredients: ${ingredients.map(ingredients => `<li>${ingredients}</li>`)}</p>
     <p>Instructons: ${meal.meals[0].strInstructions}</p>
     <button class="dislike">Dislike</button> <button class="like">Like</button>
     `);
+    console.log(meal.meals[0]);
 
     $('.dislike').on('click', function () {
         $.ajax(URL).then(function (data) {
             render(data);
         }, function (error) {
-            console.log('something went wrong')
+            console.log('something went wrong');
             console.log(error);
-        })
-    })
+        });
+    });
 
     $('.like').on('click', function () {
         $.ajax(URL).then(function (data) {
             toDO.push(meal.meals[0].strMeal);
             render(data);
             console.log(toDO);
-        })
-    })
+        });
+    });
 };
