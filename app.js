9bb8b11c1f43e233b2987f6cc8bf6ecf4b0cb62b
Stacey Brinkley <brinkleystacey12@gmail.com>
2021-12-14 14:35:08 -0500

    added functional like button

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // variables
 const URL = 'https://www.themealdb.com/api/json/v1/1/random.php';
+const toDO = [];
 
 // element references
 const $getMeal = $('#getMeal');
@@ -16,38 +17,36 @@ function handleGetMeal(evt) {
 
     $.ajax(URL).then(function (data) {
         render(data);
-         console.log(data.meals[0])
+        console.log(data.meals[0])
     }, function (error) {
         console.log('something went wrong')
         console.log(error);
     })
 };
 
-// function skipMeal(evt) {
-//     // $.ajax(URL).then(function (data) {
-//     //     render(data);
-//          console.log(data.meals[0].strMeal)
-//     // }, function (error) {
-//     //     console.log('something went wrong')
-//     //     console.log(error);
-//     // })
-// }
 
 function render(meal) {
     $('main').html(`
-    <h3> Title: ${meal.meals[0].strMeal}</h3 >
+    <h3>Title: ${meal.meals[0].strMeal}</h3>
     <img src="${meal.meals[0].strMealThumb}" alt="${meal.meals[0].strMeal}">
-    
     <p>Instructons: ${meal.meals[0].strInstructions}</p>
-    <button class="dislike">Dislike</button> <button clss="like">Like</button>
+    <button class="dislike">Dislike</button> <button class="like">Like</button>
     `);
-    $('.dislike').on('click', function() {
+
+    $('.dislike').on('click', function () {
         $.ajax(URL).then(function (data) {
             render(data);
-            // console.log(data.meals[0].strMeal)
         }, function (error) {
             console.log('something went wrong')
             console.log(error);
+        })
+    })
+
+    $('.like').on('click', function () {
+        $.ajax(URL).then(function (data) {
+            toDO.push(meal.meals[0].strMeal);
+            render(data);
+            console.log(toDO);
         })
     })
 };
