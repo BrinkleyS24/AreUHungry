f30eed25b7c69a8344daef3f2f14a895c100600e
DESKTOP-S1Q3JHV\stace <brinkleystacey12@gmail.com>
2024-05-06 22:35:18 -0400

    overhauled homepage, updated navbar, added list for saved recipes

--- app.js
+++ app.js
@@ -1,107 +1,101 @@
-const URL = 'https://www.themealdb.com/api/json/v1/1/random.php';
-const toDo = [];
+$(document).ready(function () {
+    const URL = 'https://www.themealdb.com/api/json/v1/1/random.php';
+    const likedRecipesNote = []; // Change to camelCase
+    const $likedRecipesNote = $('.quantity'); // Update selector to target the quantity span
+    const $likedRecipesDropdown = $('#likedRecipesDropdown');
+    const $getMeal = $('#getMeal');
+    const $main = $('main');
 
-const $getMeal = $('#getMeal');
-const $main = $('main');
-const $right = $('.right');
+    $getMeal.on('click', handleGetMeal);
 
-$getMeal.on('click', handleGetMeal);
+    function handleGetMeal(evt) {
+        $.ajax(URL).then(function (data) {
+            render(data);
+        }, function (error) {
+            console.log('something went wrong');
+            console.log(error);
+        });
 
-function handleGetMeal(evt) {
-    $.ajax(URL).then(function (data) {
-        render(data);
-    }, function (error) {
-        console.log('something went wrong');
-        console.log(error);
-    });
-
-    $(this).remove();
-}
-
-// Select the appropriate element(s) for $li and attach the click event handler
-$('ul').on('click', 'li', function () {
-    const meal = toDo[$(this).index()];
-    const ingredients = [];
-
-    for (let i = 1; i <= 20; i++) {
-        if (meal[`strIngredient${i}`]) {
-            ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`);
-        } else {
-            break;
-        }
+        $(this).remove();
     }
 
-    $main.html(`
-      <div class="card"> 
-        <h3 class="card-header bg-info">Meal: ${meal.strMeal}</h3>
-        <img src="${meal.strMealThumb}" class="card-img-top" alt="${meal.strMeal}">
-        <div class="card-body">
-          <ul><strong>Ingredients:</strong> 
-            ${ingredients.map(ingredient => `<li>${ingredient}</li>`).join(' ')}
-          </ul> 
-          <p><strong>Instructions:</strong> ${meal.strInstructions}</p>
-        </div>
-      </div> 
-    `);
+    $('ul').on('click', 'li', function () {
+        const meal = likedRecipesNote[$(this).index()];
+        render(meal);
+    });
+
+    function render(meal) {
+        const ingredients = [];
+
+        for (let i = 1; i <= 20; i++) {
+            if (meal.meals[0][`strIngredient${i}`]) {
+                ingredients.push(`${meal.meals[0][`strIngredient${i}`]} - ${meal.meals[0][`strMeasure${i}`]}`);
+            } else {
+                break;
+            }
+        }
+
+        $main.html(`
+            <div class="card"> 
+                <h3 class="card-header bg-info">${meal.meals[0].strMeal}</h3>
+                <img src="${meal.meals[0].strMealThumb}" class="card-img-top" alt="${meal.meals[0].strMeal}">
+                <div class="card-body">
+                    <ul><strong>Ingredients:</strong> ${ingredients.map(ingredient => `<li>${ingredient}</li>`).join(' ')}</ul> 
+                    <p><strong>Instructions:</strong> ${meal.meals[0].strInstructions}</p>
+                </div>
+                <div class="card-footer">
+                    ${likedRecipesNote.includes(meal) ?
+                        '<button class="next btn btn-primary">Next</button>' :
+                        '<button class="dislike btn btn-danger">Dislike</button> <button class="like btn btn-success">Like</button>'
+                    }
+                </div>
+            </div> 
+        `);
+
+        $('.dislike').on('click', handleGetMeal);
+
+        $('.like').on('click', function () {
+            likedRecipesNote.push(meal);
+            updateLikedRecipesNote(); // Update the quantity after adding a meal
+            renderNextMeal();
+            renderLikedRecipesDropdown();
+        });
+
+        $('.next').on('click', handleGetMeal);
+    }
+
+    function updateLikedRecipesNote() {
+        // Update the quantity span with the length of likedRecipesNote array
+        $likedRecipesNote.text(likedRecipesNote.length);
+    }
+
+    function renderLikedRecipesDropdown() {
+        $likedRecipesDropdown.empty(); // Clear previous items
+        likedRecipesNote.forEach(meal => {
+            const $dropdownItem = $(`
+                <li>
+                    <a class="dropdown-item" href="#">
+                        <img src="${meal.meals[0].strMealThumb}" alt="${meal.meals[0].strMeal}" style="width: 30px; height: 30px;">
+                        <span>${meal.meals[0].strMeal}</span>
+                    </a>
+                </li>
+            `);
+            $likedRecipesDropdown.append($dropdownItem);
+        });
+        console.log('Dropdown updated with liked recipes:', likedRecipesNote); // Check if dropdown is updated
+    }
+
+    function renderNextMeal() {
+        $.ajax(URL).then(function (data) {
+            render(data);
+        }, function (error) {
+            console.log('something went wrong');
+            console.log(error);
+        });
+    }
+
+    
+
+    
 });
 
-function render(meal) {
-    const ingredients = [];
-
-    for (let i = 1; i <= 20; i++) {
-        if (meal.meals[0][`strIngredient${i}`]) {
-            ingredients.push(`${meal.meals[0][`strIngredient${i}`]} - ${meal.meals[0][`strMeasure${i}`]}`);
-        } else {
-            break;
-        }
-    }
-
-    $('main').html(`
-    <div class="card"> 
-    <h3 class="card-header bg-info">Meal: ${meal.meals[0].strMeal}</h3>
-    <img src="${meal.meals[0].strMealThumb}" class="card-img-top" alt="${meal.meals[0].strMeal}">
-    <div class="card-body">
-    <ul><strong>Ingredients:</strong> ${ingredients.map(ingredient => `<li>${ingredient}</li>`).join(' ')}</ul> 
-    <p><strong>Instructons:</strong> ${meal.meals[0].strInstructions}</p>
-    </div>
-    <div class="card-footer">
-    ${toDo.includes(meal) ?
-        '<button class="next btn btn-primary">Next</button>' :
-        '<button class="dislike btn btn-danger">Dislike</button> <button class="like btn btn-success">Like</button>'
-    }
-    </div>
-    </div> 
-    `);
-
-    $('.dislike').on('click', handleGetMeal);
-
-    $('.like').on('click', function () {
-        toDo.push(meal);
-
-        const $ul = $('ul');
-        const $li = $(`<li class="toDoMeal">${meal.meals[0].strMeal} 
-                    <button class="delete">X</button>
-                   </li>`);
-
-        if (!$right.find('h5').length) {
-            $right.prepend('<h5>To Make List:</h5>');
-        }
-
-        $ul.append($li);
-        $li.on('click', '.delete', function () {
-            $(this).closest('li').remove();
-            if (!$right.find('li').length) {
-                $right.find('h5').remove();
-            }
-        });
-
-        $('.toDoMeal').on('click', function () {
-            const clickedMeal = toDo[$(this).index()];
-            render(clickedMeal);
-        });
-
-        handleGetMeal();
-    });
-
-    $('.next').on('click', handleGetMeal);
-}
